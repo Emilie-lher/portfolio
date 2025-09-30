da8ed365e0587dc7d7fc2e6c4afb514354332392
Emilie-lher <emilie.lher@etudiant.univ-brest.fr>
2025-09-30 14:22:56 +0200

    fichier en tete

---
+++ protfolio-emilie/src/components/Competences.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Competences() {
+  return (
+    <section>
+      <h2>Compétences</h2>
+      <ul>
+        <li><strong>Langages :</strong> HTML, CSS, JavaScript, PHP, SQL, Python, Go, Java, Kotlin, C/C++/C#</li>
+        <li><strong>Frameworks :</strong> CodeIgniter 3 & 4, Symfony, Xamarin</li>
+        <li><strong>Outils :</strong> Eclipse, VS Code, Git, Gradle</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Competences;
---
+++ protfolio-emilie/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Emilie L’Her</h1>
+      <p>Étudiante en Master Informatique – Ingénierie du logiciel</p>
+    </header>
+  );
+}
+
+export default Header;
